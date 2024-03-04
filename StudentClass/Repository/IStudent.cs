3c66fcbfad9ddfc7ccb2499c553a393430f0df1f
ChandanG80 <chandan.cg1998@gmail.com>
2024-03-04 23:37:03 +0530

    Interface added

---
+++ StudentClass/Repository/IStudent.cs
@@ -0,0 +1,19 @@
+﻿using StudentClass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentClass.Repository
+{
+    public interface IStudent
+    {
+        string AddStudent(Student student);
+        IEnumerable<Student> GetStudents();
+        Student GetStudentById(int id);
+        string UpdateStudent(int id, Student student);
+        string DeleteStudent(int id);
+        IEnumerable<Student> GetStudentByClass(int id);
+        List<Student> GetStudentList(Student student);
+    }
+}
